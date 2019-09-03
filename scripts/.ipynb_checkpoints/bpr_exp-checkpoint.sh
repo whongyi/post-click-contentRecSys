b6eb35a70c229b833b6ac270301ee7cb38ff078e
hongyi <hw557@cornell.edu>
2019-09-03 19:10:44 -0400

    remove checkpoints

--- scripts/.ipynb_checkpoints/bpr_exp-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-trap "exit" INT
-
-for l2_reg in 0.1 0.01 0.001; do
-    for p_n_ratio in 0.0 0.2 0.4 0.6 0.8 1.0; do
-        python3 bpr_implicit.py --dataset=$1 --l2_reg=$l2_reg --p_n_ratio=$p_n_ratio --log=True
-    done
-done
